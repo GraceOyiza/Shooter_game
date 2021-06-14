10963fcd0690523d7889c5ea2d671ee9c324dc1f
GraceOyiza <graceoyiza13@gmail.com>
2021-06-14 18:02:13 +0100

    Delete unused files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/GameScene.js',
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-transform-runtime',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CopyPlugin({ patterns: [{ from: 'src/assets', to: 'assets' }] }),
+  ],
+};
