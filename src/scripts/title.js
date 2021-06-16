a2e0011dde47bcd9cde0c85d93bd79653a186d45
GraceOyiza <graceoyiza13@gmail.com>
2021-06-16 11:40:48 +0100

    Add play button and links it to the game

--- src/scripts/title.js
+++ src/scripts/title.js
@@ -8,4 +8,40 @@ export default class extends Phaser.Scene {
   preload() {
     this.load.image('button', 'assets/button_hover.png');
   }
+
+  create() {
+    // Play button
+    const playButton = this.add
+      .sprite(
+        this.sections.main.centerX,
+        this.sections.main.centerY - 50,
+        'button',
+      )
+      .setInteractive();
+    const playText = this.add.text(0, 0, 'Play', {
+      fontSize: 24,
+      fill: '#fff',
+    });
+    Phaser.Display.Align.In.Center(playText, playButton);
+
+    playButton.setInteractive();
+    playButton.on('pointerup', () => {
+      if (localStorage.getItem('playerName')) {
+        this.scene.start('Game');
+      } else {
+        const playerForm = document.getElementById('playerForm');
+        playerForm.style.display = 'flex';
+        playerForm.onsubmit = (e) => {
+          e.preventDefault();
+
+          const playerName = document.getElementById('playerName').value;
+
+          localStorage.setItem('playerName', playerName);
+          playerForm.style.display = 'none';
+
+          this.scene.start('Game');
+        };
+      }
+    });
+  }
 }
