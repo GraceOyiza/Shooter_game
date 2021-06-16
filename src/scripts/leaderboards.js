fbbdb1ac1bb8496371d6c5eb16caf1d4adb38a66
GraceOyiza <graceoyiza13@gmail.com>
2021-06-16 12:38:43 +0100

    Add scoreboard

--- src/scripts/leaderboards.js
+++ src/scripts/leaderboards.js
@@ -12,4 +12,27 @@ export default class extends Phaser.Scene {
   preload() {
     this.load.image('button', 'assets/button_hover.png');
   }
+
+  create() {
+    const scoreboardList = this.add.text(0, 0, '');
+    let scoreboardText = 'RANKINGS';
+
+    scoreboard
+      .getScores()
+      .then((data) => {
+        data.result
+          .sort((a, b) => a.score - b.score)
+          .reverse()
+          .splice(0, 6)
+          .forEach((entry) => {
+            scoreboardText += `${entry.user}: ${entry.score}\n`;
+            scoreboardList.setText(scoreboardText);
+            scoreboardList.setPosition(
+              this.section.main.centerX - scoreboardList.width / 2,
+              this.section.main.centerY - scoreboardList.height / 2,
+            );
+          });
+      })
+      .catch((err) => err);
+  }
 }
