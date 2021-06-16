303e0575d6fcfb6dc371004931a6f3b16f3a9002
GraceOyiza <graceoyiza13@gmail.com>
2021-06-16 11:30:49 +0100

    Load button image

---
+++ src/scripts/title.js
@@ -0,0 +1,11 @@
+import Phaser from 'phaser';
+
+export default class extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Title' });
+  }
+
+  preload() {
+    this.load.image('button', 'assets/button_hover.png');
+  }
+}
