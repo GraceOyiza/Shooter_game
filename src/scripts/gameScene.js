0ce3f2628b63d5fda7b093c10aebb39947e29946
GraceOyiza <graceoyiza13@gmail.com>
2021-06-16 12:51:04 +0100

    Add player, enemy and laser image to preload function

--- src/scripts/gameScene.js
+++ src/scripts/gameScene.js
@@ -5,4 +5,10 @@ export default class extends Phaser.Scene {
     super({ key: 'GameScene' });
     this.baseVelocity = 160;
   }
+
+  preload() {
+    this.load.image('player', 'assets/player.png');
+    this.load.image('enemy', 'assets/enemy.png');
+    this.load.image('laser', 'assets/LaserPlayer.png');
+  }
 }
