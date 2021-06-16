2e837b045ef83880c8ce3907639a042b8d7985d0
GraceOyiza <graceoyiza13@gmail.com>
2021-06-16 10:47:16 +0100

    Add loading text to preload function

--- src/scripts/preload.js
+++ src/scripts/preload.js
@@ -4,4 +4,26 @@ export default class extends Phaser.Scene {
   constructor() {
     super({ key: 'Preload' });
   }
+
+  preload() {
+    this.load.image('playerShip', 'assets/img/playerShip.png');
+
+    this.sectionX = this.sections.main.centerX;
+    this.sectionY = this.sections.main.centerY;
+    this.sectionHeight = this.sections.main.height;
+    const loading = this.add.text(0, 0, '', { fontSize: 24 });
+
+    // update progress percentage
+    this.load.on('progress', (value) => {
+      loading.setText(`Loading... ${value * 100}%`);
+      loading.setPosition(
+        this.sectionXloading.width / 2,
+        this.sectionY + this.sectionHeight * 0.05,
+      );
+    });
+
+    setTimeout(() => {
+      this.scene.start('Title');
+    }, 1500);
+  }
 }
