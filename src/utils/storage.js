884e8e6d21ad2c2b9d853e5e92997d66b724deb5
GraceOyiza <graceoyiza13@gmail.com>
2021-06-18 17:12:40 +0100

    Save name to local storage

---
+++ src/utils/storage.js
@@ -0,0 +1,4 @@
+export const saveNameTolocalstorage = (value = 'Anonymous') =>
+  localStorage.setItem('playerName', value);
+
+export const getName = () => localStorage.getItem('playerName').toUpperCase();
